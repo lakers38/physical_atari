9b1eff0018ae75b2941839466a6fec13067bac4d
Adam Patni <adampatni27@gmail.com>
2025-11-20 00:11:27 -0800

    cleanup

--- harness_physical.py
+++ harness_physical.py
@@ -40,12 +40,12 @@ def main(args):
     logger.info(f"Importing agent: {args.agent_type}")
     if args.agent_type == 'agent_delay_target':
         from agent_delay_target import Agent
-    elif args.agent_type == 'agent_random':
-        from agent_random import Agent
     elif args.agent_type == 'agent_dqn':
         from agent_dqn import Agent
     elif args.agent_type == 'agent_rainbow':
         from agent_rainbow import Agent
+    elif args.agent_type == 'agent_random':
+        from agent_random import Agent
     else:
         raise ValueError(f"Invalid agent type={args.agent_type}")
 
